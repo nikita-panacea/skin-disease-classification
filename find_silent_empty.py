ddd41aad5ab585f9091e421906dc0f974649e024
nikita-panacea <nikita.trainee@panaceainfosec.com>
2026-04-24 11:20:00 +0530

    script to find unprocessed captions

--- find_silent_empty.py
+++ find_silent_empty.py
@@ -6,12 +6,12 @@ CKPT = Path("dedup_caption_features_v2.json")
 with CKPT.open(encoding="utf-8") as f:
     data = json.load(f)
 
-# data is {caption_text: {feature_name: 0|1|2}}
+# data is {caption_text: [int, int, ...]}  (one int per feature in all_feature_names order)
 all_unknown_substantive = []
 all_unknown_trivial = []
 
 for caption, feats in data.items():
-    if all(v == 2 for v in feats.values()):
+    if all(v == 2 for v in feats):  # <-- was feats.values(); feats is a list
         if len((caption or "").strip()) >= 15:
             all_unknown_substantive.append(caption)
         else:
